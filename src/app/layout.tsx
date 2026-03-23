39e96b262478b1d5798578f45ffa4dc5b5260b1f
saidsaoudi <said.essaoudi21@gmail.com>
2026-03-23 14:25:40 +0100

    add hotjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -213,6 +213,20 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Hotjar Tracking Code */}
+        <Script id="hotjar-snippet" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6674493,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
+
         {/* Preload critical assets for LCP */}
         <link
           rel="preload"
